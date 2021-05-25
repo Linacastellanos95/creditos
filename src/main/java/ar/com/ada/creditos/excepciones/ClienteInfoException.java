3b8bc1dcfe8294bfefbffb9c02d6fb9e8e510c12
Linacastellanos95 <linamu-ozc@hotmail.com>
2021-05-24 22:06:44 -0500

    Se crean packages y clases de credito y se pasa del Repo de AbmCliente con sus métodos.

---
+++ src/main/java/ar/com/ada/creditos/excepciones/ClienteInfoException.java
@@ -0,0 +1,14 @@
+package ar.com.ada.creditos.excepciones;
+import ar.com.ada.creditos.entities.*;
+
+public class ClienteInfoException extends Exception {
+
+    private Cliente cliente;
+
+    public ClienteInfoException(Cliente cliente, String mensaje){
+
+        super(cliente.getNombre() + ":" + mensaje);
+        this.cliente = cliente;
+    }
+    
+}
